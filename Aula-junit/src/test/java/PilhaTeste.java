27bb1683efec7a42811631bc8d05468e97dd2d04
leomendesjr <leonardo.jose@gec.inatel.br>
2022-08-15 16:33:03 -0300

    Aula-junit

---
+++ Aula-junit/src/test/java/PilhaTeste.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PilhaTeste {
+
+    @Test
+    public void testePilhaVazia(){
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+
+        boolean vazia = pilhaInteiros.pilhaVazia();
+
+        assertTrue(vazia);
+    }
+
+    @Test
+    public void testePilhaPush(){
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        pilhaInteiros.push(77);
+
+        int tamanho = pilhaInteiros.size();
+
+        assertEquals(1,tamanho);
+    }
+
+    @Test
+    public void testePilhaPopSize(){
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        pilhaInteiros.push(777);
+        pilhaInteiros.pop();
+        int tamanho = pilhaInteiros.size();
+
+        assertEquals(0,tamanho);
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void TestePilhaPopException(){
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        int retorno = pilhaInteiros.pop();
+    }
+
+    @Test
+    public void testePilhaPopRetorno(){
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        int valor = 13;
+        pilhaInteiros.push(valor);
+        int retorno = pilhaInteiros.pop();
+        assertEquals(valor, retorno);
+    }
+
+}
